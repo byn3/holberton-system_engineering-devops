d220fa7acba94bff2e9258fa81e4c5a48cac4be6
byn3 <bryanleung92@gmail.com>
2019-02-12 06:43:07 +0000

    Add: exports data into csv format

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Python script that uses rest api to fetch data """
+
+if __name__ == "__main__":
+    """ script that does what is asked above """
+    import requests
+    from sys import argv
+
+    # get the id and cast into an int
+    employee_id = int(argv[1])
+    # retrieve the user info, holds name, company json, website, phone, email,
+    # username, address, geo location, city, zip, suite, and id
+    user_info = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                           format(employee_id)).json()
+    # Gets the todo list which contains userid, id, title, completed
+    todo_list = requests.get(
+                "https://jsonplaceholder.typicode.com/todos?userId={}".
+                format(employee_id)).json()
+    completed_array = []
+    # store only the completed tasks
+    for eachTodo in todo_list:
+        if eachTodo.get("completed"):
+            completed_array.append(eachTodo.get('title'))
+    print("Employee {} is done with tasks({}/{}):".format(
+                user_info.get('name'), len(completed_array), len(todo_list)))
+    for element in completed_array:
+        print("\t {}".format(element))
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Python script that creates a csv file """
+
+if __name__ == "__main__":
+    """ script that does what is asked above 
+    and is import protected """
+    import csv
+    import requests
+    from sys import argv
+
+    # get the id and cast into an int
+    employee_id = int(argv[1])
+
+    # retrieve the user info, holds name, company json, website, phone, email,
+    # username, address, geo location, city, zip, suite, and id
+    user_info = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                           format(employee_id)).json()
+
+    # Gets the todo list which contains userid, id, title, completed
+    todo_list = requests.get(
+                "https://jsonplaceholder.typicode.com/todos?userId={}".
+                format(employee_id)).json()
+
+    # writers a csv file now with #.csv as the file name
+    with open("{}.csv".format(employee_id), "w") as f:
+        row = csv.writer(f, delimiter=",", quoting=csv.QUOTE_ALL)
+        for each_todo in todo_list:
+            row.writerow([employee_id, user_info.get("username"),
+                         each_todo.get("completed"), each_todo.get("title")])
