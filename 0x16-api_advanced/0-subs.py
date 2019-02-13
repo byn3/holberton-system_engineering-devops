0bbca2b043c33029b594a4e7561ca79f286ffa64
byn3 <bryanleung92@gmail.com>
2019-02-13 21:52:37 +0000

    Add: switched urls to a .json one

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,17 +10,17 @@ def number_of_subscribers(subreddit):
     """ does what is stated above """
     import requests
 
-    url = "https://api.reddit.com/r/{}/about".format(subreddit)
-    # url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    # url = "https://api.reddit.com/r/{}/about".format(subreddit)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     request = requests.get(url,
                            headers={'User-Agent': 'Byn'},
                            allow_redirects=False)
     # print(request)
-    if request.status_code == 404:
+    if request.status_code != 200:
         return 0
     request = request.json()
     # print(request)
     if 'data' in request:
-        print(request.get('data'))
+        # print(request.get('data'))
         return request.get('data').get('subscribers')
     return 0
