232fc2d9effc712dca1edea82b65c3de5f6022a0
byn3 <bryanleung92@gmail.com>
2019-03-08 00:45:05 +0000

    Add: initial commit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# adds tons of workers
+exec {'increase':
+    command => 'sudo sed -i "s/worker_processes 4;/worker_processes 20;worker_rlimit_nofile 3000;/" /etc/nginx/nginx.conf; sudo service nginx restart',
+    path    => '/etc/nginx/nginx.conf:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+}
