effdd185627e6ec58a3667bacdfcd3549e00383d
byn3 <bryanleung92@gmail.com>
2019-03-08 00:48:47 +0000

    Add ignore puppet lint line

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # adds tons of workers
 exec {'increase':
-    command => 'sudo sed -i "s/worker_processes 4;/worker_processes 20;worker_rlimit_nofile 3000;/" /etc/nginx/nginx.conf; sudo service nginx restart',
+    command => 'sudo sed -i "s/worker_processes 4;/worker_processes 20;worker_rlimit_nofile 3000;/" /etc/nginx/nginx.conf; sudo service nginx restart',  # lint:ignore:140chars
     path    => '/etc/nginx/nginx.conf:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 }
